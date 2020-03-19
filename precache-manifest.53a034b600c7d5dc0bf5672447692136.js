58cce51b4257a87a36f72ca9762006ec63dc3343
ericweinberg <egweinberg@viterbo.edu>
2020-03-19 16:28:42 -0500

    Updates

---
+++ precache-manifest.53a034b600c7d5dc0bf5672447692136.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aea78b4670ab0c90df7fcc8f971fd6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05ad9899b1c8a0df253a",
+    "url": "/static/css/2.b5ccfbbb.chunk.css"
+  },
+  {
+    "revision": "32b449a543d2799eaaf5",
+    "url": "/static/css/main.4f7c7918.chunk.css"
+  },
+  {
+    "revision": "05ad9899b1c8a0df253a",
+    "url": "/static/js/2.a6c8d795.chunk.js"
+  },
+  {
+    "revision": "32b449a543d2799eaaf5",
+    "url": "/static/js/main.3d88a5ab.chunk.js"
+  },
+  {
+    "revision": "cf5b8c5461a260d77c98",
+    "url": "/static/js/runtime-main.24f3d6ae.js"
+  }
+]);
